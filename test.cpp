95a534c42f645efdc251c0dfe60cc4edf81379ff
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-04-11 16:03:01 +0600

    initial commit

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pi acos(-1)
+#define endl "\n"
+#define ull unsigned long long
+
+#define inf 10002
+
+
+int main()
+{
+    FastRead;
+  
+   #ifndef sabbir
+    freopen("/CP_OJ/input.txt", "r", stdin); 
+    freopen("/CP_OJ/output.txt", "w", stdout);
+    #endif
+
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<t<<endl;
+    }
+   
+}
