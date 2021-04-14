6ec3bf113b80deddaedf3044c811d0ec1a4c0d06
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-04-14 20:18:50 +0600

    add edu107 A

---
+++ edu107/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pi acos(-1)
+#define endl "\n"
+#define ull unsigned long long
+ 
+#define inf 10002
+ 
+ 
+int main()
+{
+    FastRead;
+  
+   // #ifndef sabbir
+   //  freopen("input.txt", "r", stdin); 
+   //  freopen("output.txt", "w", stdout);
+   //  #endif
+ 
+   long long t;
+   cin>>t;
+   while(t--)
+   {
+    long long n,i,a,c=0;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+      cin>>a;
+      if(a==1 || a==3)  c++;
+    }
+    cout<<c<<endl;
+   }
+}
