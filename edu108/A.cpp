521efb41f47f5f9f38f89858b1fb7d79bf9dbe98
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-05-01 11:09:56 +0600

    edu 108 A & B

---
+++ edu108/A.cpp
@@ -0,0 +1,31 @@
+// سْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pi acos(-1)
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ull unsigned long long
+#define con (f?"YES":"NO")
+#define inf 10002
+#define MOD 1000000007
+
+int main()
+{
+    FastRead;
+    // #ifndef sabbir
+    // freopen("/CP_OJ/input.txt", "r", stdin);
+    // freopen("/CP_OJ/output.txt", "w", stdout);
+    // #endif
+
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+      long long r,b,d;
+      cin>>r>>b>>d;
+      long long minn=min(r,b),maxx=max(r,b);
+      if((maxx+minn-1)/minn>d+1 && r!=b) cout<<"NO"<<endl;
+      else cout<<"YES"<<endl;
+    }
+}
---
+++ edu108/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+#ifndef sabbir
+    freopen("/CP_OJ/input.txt", "r", stdin);
+    freopen("/CP_OJ/output.txt", "w", stdout);
+    #endif
