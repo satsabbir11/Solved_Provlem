cff8bb51d856ce60284b298273024fc787db8291
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-05-02 02:02:57 +0600

    cumilitive some repository

---
+++ Cumilitive/1253C.cpp
@@ -0,0 +1,31 @@
+//بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pi acos(-1)
+#define endl "\n"
+#define ull unsigned long long
+#define For(s,n) for(long long i=s;i<=n;i++)
+ 
+int main()
+{
+  FastRead;
+  
+    // #ifndef sabbir
+    // freopen("input.txt", "r", stdin); 
+    // freopen("output.txt", "w", stdout);
+    // #endif
+ 
+  long long n,m;
+  cin>>n>>m;
+    long long i,a[n+2],cum[n+2]={},ans[n+2]={};
+    for(i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1);
+    for(i=1;i<=n;i++) cum[i]=cum[i-1]+a[i];
+    for(i=1;i<=n;i++)
+    {
+      ans[i]=cum[i]+ans[max(0ll,i-m)];
+      cout<<ans[i]<<" ";
+    }
+}
--- test.cpp
+++ test.cpp
@@ -1,30 +1,34 @@
-//بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
-
+// سْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+ 
 #include<bits/stdc++.h>
 using namespace std;
-#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+ 
 #define pi acos(-1)
-#define endl "\n"
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define ull unsigned long long
 #define con (f?"YES":"NO")
 #define inf 10002
 #define MOD 1000000007
-
-
+ 
 int main()
 {
     FastRead;
-  
-   #ifndef sabbir
-    freopen("/CP_OJ/input.txt", "r", stdin); 
+
+
+    #ifndef sabbir
+    freopen("/CP_OJ/input.txt", "r", stdin);
     freopen("/CP_OJ/output.txt", "w", stdout);
     #endif
-
+ 
     long long t;
     cin>>t;
     while(t--)
     {
-        cout<<t<<endl;
+      long long r,b,d;
+      cin>>r>>b>>d;
+      long long minn=min(r,b),maxx=max(r,b);
+      if(maxx/minn>d && r!=b) cout<<"NO"<<endl;
+      else cout<<"YES"<<endl;
     }
-   
 }
+
