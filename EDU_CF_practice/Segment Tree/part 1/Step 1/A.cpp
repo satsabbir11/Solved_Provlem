940085b775e07fc3df39bf654671286112e72322
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-05-24 15:34:43 +0600

    optimal sol of A

--- EDU_CF_practice/Segment Tree/part 1/Step 1/A.cpp
+++ EDU_CF_practice/Segment Tree/part 1/Step 1/A.cpp
@@ -9,72 +9,75 @@ using namespace std;
 #define con (f?"YES":"NO")
 #define inf 100002
 #define MOD 1000000007
-long long tree[4*inf],a[inf];
 
-void init(long long node, long long s, long long e)
+void init(long long a[],long long tree[],long long node, long long s, long long e)
 {
   if(s==e)
   {
-    tree[node]=a[s];
+    tree[node]=a[e];
     return;
   }
-  long long left=node*2, right= 1+node*2, mid=(s+e)/2;
-  init(left,s,mid);
-  init(right,mid+1,e);
+  long long left=2*node, right=left+1, mid=(s+e)/2;
+  init(a,tree, left, s, mid);
+  init(a,tree, right, mid+1, e);
   tree[node]=tree[right]+tree[left];
 }
 
-long long func(long long node, long long s, long long e, long long i, long long j)
+void update(long long a[],long long tree[],long long node, long long s, long long e, long long in, long long v)
 {
-  if(s>j || i>e) return 0;
-  if(s>=i && j>=e) return tree[node];
-  long long left=node*2, right= 1+node*2, mid=(s+e)/2;
-  long long p=func(left,s,mid,i,j), q=func(right,mid+1,e,i,j);
-  return p+q;
-}
-
-void update(long long node, long long s, long long e, long long in, long long v)
-{
- if(in<s || e<in) return;
-  if(in<=s && e<=in)
+  if(in<s || e<in) return;
+  if(s==e && s==in)
   {
     tree[node]=v;
     return;
   }
-  long long left=node*2, right= 1+node*2, mid=(s+e)/2;
-  update(left,s,mid,in,v);
-  update(right,mid+1,e,in,v);
+
+  long long left=2*node, right=left+1, mid=(s+e)/2;
+  update(a, tree, left, s, mid, in, v);
+  update(a, tree, right, mid+1, e, in, v);
   tree[node]=tree[right]+tree[left];
+}
+
+long long func(long long a[],long long tree[],long long node, long long s ,long long e, long long i, long long j)
+{
+  if(j<s || e<i) return 0;
+  if(i<=s && e<=j) return tree[node];
+
+  long long left=2*node, right=left+1, mid=(s+e)/2;
+  long long p=func(a, tree, left, s, mid, i, j), q=func(a, tree, right, mid+1, e, i, j);
+  return p+q;
 }
 
 
 int main()
 {
-  // #ifndef sabbir
-  //   freopen("input.txt", "r", stdin); 
-  //   freopen("output.txt", "w", stdout);
-  //   #endif
+    FastRead;
+  
+   // #ifndef sabbir
+   //  freopen("input.txt", "r", stdin); 
+   //  freopen("output.txt", "w", stdout);
+   //  #endif
 
-
-  long long n,m;
-  cin>>n>>m;
-  for(long long i=1;i<=n;i++) cin>>a[i];
-  init(1,1,n);
-  while(m--)
-  {
-    long long op;
-    cin>>op;
-    if(op==1)
+    long long n,m,i;
+    cin>>n>>m;
+    long long a[n+2],tree[4*n];
+    for(i=1;i<=n;i++) cin>>a[i];
+    init(a, tree, 1,1,n);
+    while(m--)
     {
-      long long in,v;
-      cin>>in>>v;
-      update(1,1,n,in+1,v);
+      long long p;
+      cin>>p;
+      if(p==2)
+      {
+        long long x,y;
+        cin>>x>>y;
+        cout<<func(a, tree, 1,1,n,x+1,y)<<endl;
+      }
+      else
+      {
+        long long x,y;
+        cin>>x>>y;
+        update(a, tree, 1,1,n,x+1,y);
+      }
     }
-    else
-    {
-      long long ss,ee;
-      cin>>ss>>ee;
-      cout<<func(1,1,n,ss+1,ee)<<endl;
-    }
-  }
 }
