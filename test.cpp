b1f6495ea9579288488bac33ae0aa83fcdd67252
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-03-12 22:38:28 +0600

    cf776 D.cpp

--- test.cpp
+++ test.cpp
@@ -34,98 +34,38 @@ int main(){
   long long t;
   cin>>t;
   while(t--){
-    long long n,ans=0;
+    long long n;
     cin>>n;
-    if(n==1){
-        cout<<1<<endl;
-        continue;
+    long long in,tt=n;
+    long long a[n+2];
+    for(long long i=1;i<=n;i++){
+        cin>>a[i];
+        if(a[i]==n) in=i;
     }
-    if(n==2){
-        cout<<2<<endl;
-        continue;
+    //cout<<in<<endl;
+    //continue;
+    vector<long long>p;
+    long long cur=n;
+    while(1){
+        long long f=in%cur;
+        p.push_back(f);
+        
+        if(cur==1) break;
+        //cout<<cur<<endl;
+        long long gg,b[cur+2],k=1;
+        for(long long i=in+1;i<=cur;i++) b[k++]=a[i];
+        for(long long i=1;i<=in;i++) b[k++]=a[i];
+        //cout<<gg<<endl;
+        //in=gg;
+            for(long long i=1;i<=cur;i++){
+                if(b[i]==(cur-1)) in=i;
+                a[i]=b[i];
+            }
+        //for(long long i=1;i<=n;i++) cout<<a[i]<<" ";
+        cur--;
+        //cout<<endl;
     }
-    if(n%2){
-        if(n%3==2){
-            for(long long i=1;i<=n;i++){
-            if(i%2){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-        else if(n%3==1){
-            for(long long i=1;i<=n;i++){
-            if(i%2==0){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-        else{
-            for(long long i=1;i<=n;i++){
-            if(i%2==1){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-    }
-    else{
-        if(n%3==2){
-            for(long long i=1;i<=n;i++){
-            if(i%2==1){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-        else if(n%3==1){
-            for(long long i=1;i<=n;i++){
-            if(i%2==0){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-        else{
-            for(long long i=1;i<=n;i++){
-            if(i%2){
-                cout<<2;
-                ans+=2;
-            }
-            else{
-                cout<<1;
-                ans++;
-            }
-            if(ans==n) break;
-        }
-        }
-    }
+    for(long long i=n-1;i>=0;i--) cout<<p[i]<<" ";
     cout<<endl;
   }
 }
