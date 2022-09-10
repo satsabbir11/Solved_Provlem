60c102ab2965302db09e9f21a28b9cff55f8cee4
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-09-10 17:57:11 +0600

    remove work-shop-dir

---
+++ Codechef_Contest/Long_Challenge/september-22/B.cpp
@@ -0,0 +1,39 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ #define FastRead ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
+ #define pi acos(-1)
+ #define MOD 1000000007
+ #define inf 1000010
+ #define endl "\n"
+ #define ull unsigned long long
+ #define con (f?"YES":"NO")
+ 
+ #define Dpos(n) fixed << setprecision(n)
+ 
+ #define CLR(a) memset(a, -1, sizeof(a))
+ #define CLN(a) memset(a, 0, sizeof(a))
+ 
+ #define max3(a,b,c) max(max(a,b),c)
+ #define min3(a,b,c) min(min(a,b),c)
+ #define max4(a,b,c,d) max(a,max3(b,c,d))
+ #define min4(a,b,c,d) min(a,min3(b,c,d))
+ #define max5(a,b,c,d,e) max(max4(a,b,c,d),e)
+ #define min5(a,b,c,d,e) min(min4(a,b,c,d),e)
+ 
+ #define sortn(a,n,m) sort(a+m, s+m+n)
+ #define sortt(s) sort(s.begin(), s.end())
+ #define reversee(s) reverse(s.begin(), s.end())
+ #define reversesortt(s) sortt(s); reversee(s)
+ #define pb push_back
+ #define loj(i,j) "Case "<<i<<": "<<j
+ #define gap " "
+ 
+ // for (auto& x : a) cin >> x;
+ 
+ int main() {
+     FastRead
+ 
+     a[]={2,3,5,4,1};
+
+     for
+ }
