15a49073275e698254729c37881a2c2e1a3ef211
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-09-11 15:14:11 +0600

    comment out all problems

--- Class MU/Basic/test-1.cpp
+++ Class MU/Basic/test-1.cpp
@@ -1,34 +1,48 @@
 #include <stdio.h>
 
 int main(){
-
+	
+    //print Hello World
 	printf("Hello World\n");
 
 	int a,b;
+    
 
+    //Add 1 and 2
 	a=1, b=2;
 
 	printf("%d\n",a+b);
 
+
+	//Subtract 2 from 3
 	a=3, b=2;
 
 	printf("%d\n",a-b);
 
+	//Multiply 4 with 5
 	a=4, b=5;
 
 	printf("%d\n",a*b);
 
+
+	//Divide 6 by 2
 	a=6, b=2;
 
 	printf("%d\n",a/b);
 
+
+	//MOD 20 and 3
 	a=20, b=3;
 
 	printf("%d\n",a%b);
 
+
+	//pre increment
 	printf("%d\n",++a);
 
+	//post increment
 	printf("%d\n",a++);
 
+	//last value of a
 	printf("%d\n",a);
 }
