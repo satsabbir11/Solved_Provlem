7b9d086e49bb0ae4cad4eb276509b49b76f2843c
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2022-09-11 14:58:54 +0600

    basic class-1

---
+++ Class MU/Basic/test-1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("Hello World\n");
+
+	int a,b;
+
+	a=1, b=2;
+
+	printf("%d\n",a+b);
+
+	a=3, b=2;
+
+	printf("%d\n",a-b);
+
+	a=4, b=5;
+
+	printf("%d\n",a*b);
+
+	a=6, b=2;
+
+	printf("%d\n",a/b);
+
+	a=20, b=3;
+
+	printf("%d\n",a%b);
+
+	printf("%d\n",++a);
+
+	printf("%d\n",a++);
+
+	printf("%d\n",a);
+}
