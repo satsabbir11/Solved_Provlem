ec4943b939b743e8efd7d7e290d2e5c50c829f61
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-01-04 18:04:38 +0600

    Code arranging

--- LeetCode/Monthly Chalange/January-23/2244. Minimum Rounds to Complete All Tasks.py
+++ LeetCode/Monthly Chalange/January-23/2244. Minimum Rounds to Complete All Tasks.py
@@ -8,13 +8,12 @@ class Solution:
         
         ans=0 
         set_tasks = set(tasks)
+        
         for task in set_tasks:
             if task_counts[task] == 1:
                 return -1
             elif task_counts[task] % 3 == 0:
                 ans += task_counts[task]//3
-            elif task_counts[task] % 3 == 1:
-                ans += (((task_counts[task]-4)//3)+2)
             else:
-                ans += ((task_counts[task]//3)+1)
+                ans += task_counts[task]//3+1
         return ans
