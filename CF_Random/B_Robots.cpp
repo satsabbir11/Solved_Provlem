d2431022e41ea683786fe8d449afad438c80d0b1
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-01-20 21:47:33 +0600

    cf random

---
+++ CF_Random/B_Robots.cpp
@@ -0,0 +1,56 @@
+//بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define FastRead ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pi acos(-1)
+#define MOD 1000000007
+#define inf 1000010
+#define endl "\n"
+#define ull unsigned long long
+#define con (ff?"YES":"NO")
+
+#define CLR(a) memset(a, -1, sizeof(a))
+#define CLN(a) memset(a, 0, sizeof(a))
+ 
+#define max3(a,b,c) max(max(a,b),c)
+#define min3(a,b,c) min(min(a,b),c)
+#define max4(a,b,c,d) max(a,max3(b,c,d))
+#define min4(a,b,c,d) min(a,min3(b,c,d))
+#define max5(a,b,c,d,e) max(max4(a,b,c,d),e)
+#define min5(a,b,c,d,e) min(min4(a,b,c,d),e)
+ 
+#define sortt(s) sort(s.begin(), s.end())
+#define reversee(s) reverse(s.begin(), s.end())
+#define reversesortt(s) sortt(s); reversee(s)
+#define pb push_back
+#define loj(i,j) "Case "<<i<<": "<<j
+#define gap " "
+ 
+// for (auto& x : a) cin >> x;
+
+
+int main(){
+  long long t;
+  cin>>t;
+  while(t--){
+    long long n,m,mem=-1;
+    bool f=true, ff=true;
+    cin>>n>>m;
+    for(long long i=1;i<=n;i++){
+      string s;
+      cin>>s;
+
+      for(long long j=0;j<m;j++){
+        if(s[j]=='R' && f){
+          mem=j;
+          f=false;
+          break;
+        }
+        if(!f && s[j]=='R' && mem>j) ff=false;
+      }
+    }
+    //cout<<mem<<endl;
+    cout<<con<<endl;
+  }
+}
