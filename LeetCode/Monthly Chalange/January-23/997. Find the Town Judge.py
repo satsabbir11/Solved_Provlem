517725aa649868de4ff296ed673d098fd85ac86c
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-01-24 02:59:29 +0600

    More optimal

--- LeetCode/Monthly Chalange/January-23/997. Find the Town Judge.py
+++ LeetCode/Monthly Chalange/January-23/997. Find the Town Judge.py
@@ -1,13 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        cnt = 0 
-        mp,m = [],[]
-        for i in range(len(trust)):
-            mp.append(trust[i][1])
-            m.append(trust[i][0])
-            
-            
-        for i in range (1,n+1):
-            if (mp.count(i)==n-1 and m.count(i)==0):
+        cnt = [0 for _ in range(n+1)]
+        for i, j in trust:
+            cnt[i] -= 1
+            cnt[j] += 1
+        
+        for i in range(1, n+1):
+            if cnt[i] == n-1:
                 return i
         return -1
