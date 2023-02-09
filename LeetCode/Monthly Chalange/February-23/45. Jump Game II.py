c72c856fd32e1f873d7b4a07660f3c96d191e7ab
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-02-10 02:42:54 +0600

    Jump first to last in a array leetcode

---
+++ LeetCode/Monthly Chalange/February-23/45. Jump Game II.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        cur =0
+        ans=0
+        i=0
+        while cur+1<len(nums):
+            ans+=1
+            now = cur
+            for j in range(i,now+1):
+                i+=1
+                cur = max(cur,j+nums[j])
+        return ans
+        
