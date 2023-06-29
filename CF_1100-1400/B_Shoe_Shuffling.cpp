5045a55f321f3ecea586715af2c64f676da30045
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-06-30 01:35:32 +0600

    Rotating vector related problem

---
+++ CF_1100-1400/B_Shoe_Shuffling.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi acos(-1)
+#define MOD 1000000007
+#define inf 1000000010
+#define endl "\n"
+#define ull unsigned long long
+#define con (f ? "YES" : "NO")
+
+#define Dpos(n) fixed << setprecision(n)
+
+#define CLR(a) memset(a, -1, sizeof(a))
+#define CLN(a) memset(a, 0, sizeof(a))
+
+#define max3(a, b, c) max(max(a, b), c)
+#define min3(a, b, c) min(min(a, b), c)
+#define max4(a, b, c, d) max(a, max3(b, c, d))
+#define min4(a, b, c, d) min(a, min3(b, c, d))
+#define max5(a, b, c, d, e) max(max4(a, b, c, d), e)
+#define min5(a, b, c, d, e) min(min4(a, b, c, d), e)
+
+#define sortn(a, x, y) sort(a + x, a + x + y)
+#define sortv(s) sort(s.begin(), s.end())
+#define reversev(s) reverse(s.begin(), s.end())
+#define reversesortv(s) \
+    sortv(s);           \
+    reversev(s)
+#define unik(a) unique(a.begin(), a.end()) - a.begin()
+
+#define lowerbound(v, x) lower_bound(v.begin(), v.end(), x) - v.begin() + 1
+#define upperbound(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+
+#define pb push_back
+#define loj(i, j) "Case " << i << ": " << j
+#define gap " "
+
+#define auto(x, a) for (auto &x : a)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        vector<long long>a(n), p(n);
+
+        for(long long i=0; i<n; i++){
+            cin>>a[i];
+            p[i] = i+1;
+        }
+
+        long long l = 0, r = 0;
+        bool f = true;
+
+        while(r<n){
+            while(r<n-1 && a[r]==a[r+1]) r++;
+            if(l==r) f=false;
+            else{
+                rotate(p.begin()+l, p.begin()+r, p.begin()+r+1);
+                l = r+1;
+            }
+            r++;
+        }
+
+        if(!f){
+            cout<<-1<<endl;
+            continue;
+        }
+
+        for(auto& x:p) cout<<x<<" ";
+        cout<<endl;
+    }
+}
