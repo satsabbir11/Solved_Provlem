9d34801d4297b2dfd9f8752df8603efa354b87c3
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-07-20 01:03:53 +0600

    Dijkstra for matrix

--- Vjudge_Contest/Dijkstra-549988/H - Number Maze .cpp
+++ Vjudge_Contest/Dijkstra-549988/H - Number Maze .cpp
@@ -1,57 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n, m;
-long long edges[1009][1009];
-long long dist[1009][1009];
+int n, m;
+int edges[1009][1009];
+int dist[1009][1009];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
-void dijkstra(long long x, long long y, long long w)
-{
-    if (dist[x][y] < w)
-        return;
+typedef pair<int, pair<int, int>> pi;
+
+
+void dijkstra(int x, int y, int w) {
+
+   priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+    pq.push({w, {x, y}});
     dist[x][y] = w;
 
-    for (int i = 0; i < 4; i++)
-    {
-        long long new_x = dx[i] + x, new_y = dy[i] + y;
+    while (!pq.empty()) {
+        int curr_w = pq.top().first;
+        int curr_x = pq.top().second.first;
+        int curr_y = pq.top().second.second;
+        pq.pop();
 
-        if (new_x <= n && new_x >= 1 && new_y <= m && new_y >= 1)
-        {
-            long long new_w = dist[x][y] + edges[new_x][new_y];
+        if (curr_w > dist[curr_x][curr_y])
+            continue;
 
-            if (new_w < dist[new_x][new_y])
-            {
-                dist[new_x][new_y] = new_w;
-                dijkstra(new_x, new_y, dist[new_x][new_y]);
+        for (int i = 0; i < 4; i++) {
+            int new_x = dx[i] + curr_x;
+            int new_y = dy[i] + curr_y;
+
+            if (new_x <= n && new_x >= 1 && new_y <= m && new_y >= 1) {
+                int new_w = curr_w + edges[new_x][new_y];
+
+                if (new_w < dist[new_x][new_y]) {
+                    dist[new_x][new_y] = new_w;
+                    pq.push({new_w, {new_x, new_y}});
+                }
             }
         }
     }
 }
 
+
 int main()
 {
-    long long t;
+    ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
+
+
+    int t;
     cin >> t;
 
-    for (long long k = 1; k <= t; k++)
+    for (int k = 1; k <= t; k++)
     {
 
         cin >> n >> m;
 
-        for (long long i = 0; i <= n + 2; i++)
+        for (int i = 0; i <= n + 2; i++)
         {
-            for (long long j = 0; j <= m + 2; j++)
+            for (int j = 0; j <= m + 2; j++)
             {
-                // edges[i][j] = 0;
-                dist[i][j] = 1e12;
+                dist[i][j] = 1<<30;
             }
         }
 
-        for (long long i = 1; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            for (long long j = 1; j <= m; j++)
+            for (int j = 1; j <= m; j++)
                 cin >> edges[i][j];
         }
 
@@ -60,3 +75,4 @@ int main()
         cout << dist[n][m] << endl;
     }
 }
+
