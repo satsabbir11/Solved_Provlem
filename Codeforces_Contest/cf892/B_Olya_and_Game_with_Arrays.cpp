b82c076ed8465012c89e02fba8c6860894188f28
tsabbir007 <satsabbir11@gmail.com>
2023-08-13 08:40:11 +0600

    using stl

--- Codeforces_Contest/cf892/B_Olya_and_Game_with_Arrays.cpp
+++ Codeforces_Contest/cf892/B_Olya_and_Game_with_Arrays.cpp
@@ -1,36 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define pi acos(-1)
-#define MOD 1000000007
-#define inf 1000000010
-#define endl "\n"
-#define ull unsigned long long
-#define con (f?"YES":"NO")
 
-#define Dpos(n) fixed << setprecision(n)
-
-#define CLR(a) memset(a, -1, sizeof(a))
-#define CLN(a) memset(a, 0, sizeof(a))
-
-#define max3(a,b,c) max(max(a,b),c)
-#define min3(a,b,c) min(min(a,b),c)
-#define max4(a,b,c,d) max(a,max3(b,c,d))
-#define min4(a,b,c,d) min(a,min3(b,c,d))
-#define max5(a,b,c,d,e) max(max4(a,b,c,d),e)
-#define min5(a,b,c,d,e) min(min4(a,b,c,d),e)
-
-#define sortn(a,x,y) sort(a+x, a+x+y)
-#define sortv(s) sort(s.begin(), s.end())
-#define reversev(s) reverse(s.begin(), s.end())
-#define reversesortv(s) sortv(s); reversev(s)
-#define unik(a) unique(a.begin(), a.end()) - a.begin()
-
-#define lowerbound(v,x) lower_bound(v.begin(), v.end(), x)-v.begin()
-#define upperbound(v,x) upper_bound(v.begin(), v.end(), x)-v.begin()
-
-#define pb push_back
-#define loj(i,j) "Case "<<i<<": "<<j
-#define gap " "
+#define all(s) s.begin(), s.end()
 
 #define auto(x,a) for (auto& x : a)
 
@@ -42,53 +13,21 @@ int main() {
     while (t--) {
         long long n;
         cin>>n;
-        
-        long long a[n+2][2];
-        for(long long i=1;i<=n;i++){
-            a[i][0]=1e12;
-            a[i][1] =1e12;
-        }
-        for(long long i=1;i<=n;i++){
+        long long mn1 = INT_MAX, ans = 0, mn2 = mn1;
+        while(n--){
             long long m;
             cin>>m;
 
-            if(m==2){
-                long long b[2];
-                cin>>b[0]>>b[1];
-                a[i][1]=max(b[0], b[1]);
-                a[i][0]=min(b[0], b[1]);
-                continue;
-            }
-            
-            for(long long j=1;j<=m; j++){
-                long long x;
-                cin>>x;
-                if(a[i][1]>x){
-                    long long tem = a[i][1];
-                    a[i][1] = x;
-                    a[i][0] = tem;
-                }
-                else{
-                    if(x>=a[i][1] && a[i][0]>=x) a[i][0] = x;
-                }
-                
-            }
-            swap(a[i][0], a[i][1]);
-        }
+            vector<long long>v(m);
+            auto(x, v) cin>>x;
 
-        long long ans =0;
-        long long mn = 1e12;
-
-        for(long long i=1;i<=n;i++){
-            ans+=a[i][1];
-            mn = min(a[i][1], mn);
+            long long mn = *min_element(all(v));
+            v.erase(find(all(v), mn));
+            mn1 = min(mn, mn1);
+            mn = *min_element(all(v));
+            ans+=mn;
+            mn2 = min(mn, mn2);
         }
-        ans-=mn;
-        mn = 1e12;
-        for(long long i=1;i<=n;i++){
-            mn = min(a[i][0], mn);
-        }
-        ans+=mn;
-        cout<<ans<<endl;
+        cout<<ans+mn1-mn2<<endl;
     }
 }
