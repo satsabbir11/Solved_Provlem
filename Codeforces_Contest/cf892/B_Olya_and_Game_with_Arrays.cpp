3940287f1dc85b2cfde23b01d198ddb958eb3422
tsabbir007 <satsabbir11@gmail.com>
2023-08-13 08:40:26 +0600

    using sorting

--- Codeforces_Contest/cf892/B_Olya_and_Game_with_Arrays.cpp
+++ Codeforces_Contest/cf892/B_Olya_and_Game_with_Arrays.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define all(s) s.begin(), s.end()
 
 #define auto(x,a) for (auto& x : a)
+#define sortv(s) sort(s.begin(), s.end())
+
 
 int main() {
    ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
@@ -20,13 +22,10 @@ int main() {
 
             vector<long long>v(m);
             auto(x, v) cin>>x;
-
-            long long mn = *min_element(all(v));
-            v.erase(find(all(v), mn));
-            mn1 = min(mn, mn1);
-            mn = *min_element(all(v));
-            ans+=mn;
-            mn2 = min(mn, mn2);
+            sortv(v);
+            ans+=v[1];
+            mn1=min(mn1, v[0]);
+            mn2=min(mn2, v[1]);
         }
         cout<<ans+mn1-mn2<<endl;
     }
