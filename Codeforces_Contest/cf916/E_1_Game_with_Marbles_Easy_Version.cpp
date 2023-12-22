fc7a14de289a6f52ef3b19c1e633c7f3de04b0d3
tsabbir007 <satsabbir11@gmail.com.com>
2023-12-22 19:47:24 +0600

    Another version

--- Codeforces_Contest/cf916/E_1_Game_with_Marbles_Easy_Version.cpp
+++ Codeforces_Contest/cf916/E_1_Game_with_Marbles_Easy_Version.cpp
@@ -1,33 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pi acos(-1)
-#define MOD 1000000007
-#define inf 1000000010
-#define endl "\n"
-#define ull unsigned long long
-#define con (f ? "YES" : "NO")
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-
-#define dpos(n) fixed << setprecision(n)
-
-#define CLR1(a) memset(a, -1, sizeof(a))
-#define CLR0(a) memset(a, 0, sizeof(a))
-
-#define sortn(a, x, n) sort(a + x, a + x + n)
-#define sortv(s) sort(s.begin(), s.end())
-#define reversev(s) reverse(s.begin(), s.end())
-#define rsortv(s) sort(s.rbegin(), s.rend())
-#define unik(a) unique(a.begin(), a.end()) - a.begin()
-
-#define lowerbound(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
-#define upperbound(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
-
-#define pb push_back
-#define loj(i, j) "Case " << i << ": " << j
-#define gap " "
-
-#define auto(x, a) for (auto &x : a)
 
 long long dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
 long long dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
@@ -41,32 +13,33 @@ int main()
     cin >> t;
     while (t--)
     {
-        long long n, ans=0;
+        long long n, ans = 0;
         cin >> n;
 
-        long long a[n + 2], b[n + 2];
+        vector<pair<int, int>> p(n);
 
-        for(long long i=0;i<n;i++) cin>>a[i];
-        for(long long i=0;i<n;i++) cin>>b[i];
+        for (int i = 0; i < n; i++)
+            cin >> p[i].first;
+        for (int i = 0; i < n; i++)
+            cin >> p[i].second;
 
-        vector<int>v(n);
+        sort(p.begin(), p.end(), [&](pair<int, int> x, pair<int, int> y) {
+            return x.first + x.second > y.first + y.second;
+        }); // main logic
 
-        iota(v.begin(), v.end(), 0);
+        int g = 0;
 
-
-        sort(v.begin(), v.end(),[&](long long x, long long y){
-            return a[x]+b[x]>a[y]+b[y];
-        });
-
-        long long g=0;
-        for(long long i: v){
-
-            //cout<<a[i]<<" "<<b[i]<<endl;
-            if(g&1) ans-=b[i]-1;
-            else ans+=a[i]-1;
-
+        for (auto x : p)
+        {
+            if (g & 1)
+                ans -= x.second - 1;
+            else
+                ans += x.first - 1;
             g++;
         }
-        cout<<ans<<endl;
+
+        cout << ans << endl;
     }
+
+    return 0;
 }
