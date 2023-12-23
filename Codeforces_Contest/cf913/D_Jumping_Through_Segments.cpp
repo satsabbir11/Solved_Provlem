ff39c521e94ec51457b7877dabd6302d23b2c027
tsabbir007 <satsabbir11@gmail.com.com>
2023-12-23 07:38:30 +0600

    initital commit

--- Codeforces_Contest/cf913/D_Jumping_Through_Segments.cpp
+++ Codeforces_Contest/cf913/D_Jumping_Through_Segments.cpp
@@ -4,6 +4,17 @@ using namespace std;
 #define con (f?"YES":"NO")
 #define loj(i,j) "Case "<<i<<": "<<j
 
+bool check(vector<int>&ll, vector<int>&rr, int n, int k){
+    int l, r;
+    l=r=0;
+    for(int i=0;i<n;i++){
+        l = max(l-k, ll[i]);
+        r = min(r+k, rr[i]);
+        if(l>r) return false;
+    }
+    return true;
+}
+
 int main() {
    ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
 
@@ -12,5 +23,18 @@ int main() {
     while (t--) {
         long long n;
         cin>>n;
+        vector<int>ll(n), rr(n);
+
+        for(int i =0;i<n;i++) cin>>ll[i]>>rr[i];
+
+        int l = 0, r = 1e9;
+
+        while(l<r ){
+            int mid = l + (r-l)/2;
+            if(check(ll, rr, n, mid)) r = mid;
+            else l = mid+1;
+        }
+
+        cout<<r<<endl;
     }
 }
