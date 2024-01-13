4410329ad747bd9032e3561e0790b209acaac400
tsabbir007 <satsabbir11@gmail.com.com>
2024-01-13 08:19:16 +0600

    Initial commit

---
+++ Codeforces_Contest/cf918/F_Greetings.cpp
@@ -0,0 +1,76 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+#define pi acos(-1)
+#define MOD 1000000007
+#define inf 1000000010
+#define endl "\n"
+#define ull unsigned long long
+#define con (f ? "YES" : "NO")
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+#define dpos(n) fixed << setprecision(n)
+
+#define clear1(a) memset(a, -1, sizeof(a))
+#define clear0(a) memset(a, 0, sizeof(a))
+
+#define sortn(a, x, n) sort(a + x, a + x + n)
+#define sortv(s) sort(s.begin(), s.end())
+#define reversev(s) reverse(s.begin(), s.end())
+#define rsortv(s) sort(s.rbegin(), s.rend())
+#define unik(a) unique(a.begin(), a.end()) - a.begin()
+#define iotav(s, x) iota(s.begin(), s.end(), x)
+
+#define lowerbound(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
+#define upperbound(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+
+#define pb push_back
+#define loj(i, j) "Case " << i << ": " << j
+#define gap " "
+
+#define auto(x, a) for (auto &x : a)
+
+long long dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+long long dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+
+struct p
+{
+    int s, e;
+};
+
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        p a[n + 2];
+        for (long long i = 1; i <= n; i++)
+            cin >> a[i].s >> a[i].e;
+        sort(a + 1, a + 1 + n, [](p x, p y)
+             { return x.e < y.e; });
+
+        ordered_set s;
+        long long cnt = 0;
+
+        for (long long i = 1; i <= n; i++)
+        {
+            s.insert(a[i].s);
+
+            long long index = s.order_of_key(a[i].s);
+            cnt += (i - index - 1);
+        }
+        cout << cnt << endl;
+    }
+}
