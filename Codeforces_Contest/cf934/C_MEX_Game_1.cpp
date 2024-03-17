63f39b6b7cc25dc12692cc4cb5683cddcaf796ba
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2024-03-17 23:16:18 +0600

    Correct solution

--- Codeforces_Contest/cf934/C_MEX_Game_1.cpp
+++ Codeforces_Contest/cf934/C_MEX_Game_1.cpp
@@ -1,83 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pi acos(-1)
-#define MOD 1000000007
-#define inf 1000000010
 #define endl "\n"
-#define ull unsigned long long
-#define con (f ? "YES" : "NO")
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-
-#define dpos(n) fixed << setprecision(n)
-
-#define clear1(a) memset(a, -1, sizeof(a))
-#define clear0(a) memset(a, 0, sizeof(a))
-
-#define sortn(a, x, n) sort(a + x, a + x + n)
-#define sortv(s) sort(s.begin(), s.end())
-#define reversev(s) reverse(s.begin(), s.end())
-#define rsortv(s) sort(s.rbegin(), s.rend())
-#define unik(a) unique(a.begin(), a.end()) - a.begin()
-#define iotav(s, x) iota(s.begin(), s.end(), x)
-
-#define lowerbound(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
-#define upperbound(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
-
-#define pb push_back
-#define loj(i, j) "Case " << i << ": " << j
-#define gap " "
-
-#define auto(x, a) for (auto &x : a)
-#define print(x) cout << #x << " = " << x << endl
-
-#define moveForward(current, ammount, mod) (amount - current) % mod
-#define moveBackward(current, ammount, mod) (((amount - current) % mod) + mod) % mod
-
-long long dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
-long long dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin.tie(0), cout.tie(0);
 
-    long long t;
-    cin >> t;
-    while (t--)
+  long long t;
+  cin >> t;
+  while (t--)
+  {
+    long long n;
+    cin >> n;
+
+    long long a[n + 2]; a[0] = -1;
+    map<long long, long long> mp;
+
+    for (long long i = 1; i <= n; i++)
     {
-        long long n;
-        cin >> n;
-
-        long long a[n + 2];
-        map<long long, long long> mp;
-        for (long long i = 1; i <= n; i++){
-            cin >> a[i];
-            mp[a[i]]++;
-        }
-
-        long long past = 0;
-        bool f = true;
-        sort(a+1, a+1+n);
-
-        for(long i=2;i<=n;i++){
-           if(mp[a[i]]==1 && !f){
-             past=a[i];
-             break;
-           }
-           if(mp[a[i]]==1) f=false;
-           if(a[i]-a[i-1]>1){
-             past = a[i-1]+1;
-             break;
-           }
-           if(mp[a[i-1]]==1 and mp[a[i]]==1){
-             past = a[i];
-             break;
-           }
-           past = a[i]+1;
-        }
-
-        if(a[1]==1) past=0;
-        cout<<past<<endl;
+      cin >> a[i];
+      mp[a[i]]++;
     }
+    sort(a + 1, a + 1 + n);
+
+    long long past = 0;
+    bool f = true;
+
+    for (long i = 1; i <= n; i++)
+    {
+      if (a[i] - a[i - 1] > 1) //if missing a number
+      {
+        past = a[i - 1] + 1;
+        break;
+      }
+
+
+      if (mp[a[i]] == 1) // no two single frequency number eg: 0 1 1 2 2 3 4 // so loop break in 3 // ans is 2+1 = 3
+      {
+        if (!f)
+        {
+          past = a[i];
+          break;
+        }
+        f = false;
+      }
+      past = a[i] + 1; //ans is always (where we can reach + 1)
+    }
+
+    cout << past << endl;
+  }
 }
