75f07364d0de4397ccba20eccc79dc264bbf32b4
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2024-03-29 16:50:46 +0600

    Solved

--- Codeforces_Contest/cf937/E_Nearly_Shortest_Repeating_Substring.cpp
+++ Codeforces_Contest/cf937/E_Nearly_Shortest_Repeating_Substring.cpp
@@ -1,43 +1,69 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define pi acos(-1)
+#define MOD 1000000007
+#define inf 1000000010
+#define ull unsigned long long
+#define con (f?"YES":"NO")
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
 
-using i64 = long long;
+#define dpos(n) fixed << setprecision(n)
 
-void solve() {
-    int n;
-    std::cin >> n;
-    
-    std::string s;
-    std::cin >> s;
-    
-    for (int len = 1; len <= n; len++) {
-        if (n % len != 0) {
-            continue;
-        }
-        
-        for (int i = 0; i < n && i <= len; i += len) {
-            std::cout<<i<<" "<<len<<std::endl;
-            int ans = 0;
-            for (int j = 0; j < n; j++) {
-                ans += (s[i + j % len] != s[j]);
-            }
-            if (ans <= 1) {
-                std::cout << len << "\n";
-                return;
-            }
-        }
-    }
-}
+#define clear1(a) memset(a, -1, sizeof(a))
+#define clear0(a) memset(a, 0, sizeof(a))
+
+#define sortn(a,x,n) sort(a+x, a+x+n)
+#define sortv(s) sort(s.begin(), s.end())
+#define reversev(s) reverse(s.begin(), s.end())
+#define rsortv(s) sort(s.rbegin(),s.rend())
+#define unik(a) unique(a.begin(), a.end()) - a.begin()
+#define iotav(s, x) iota(s.begin(), s.end(), x)
+
+#define lowerbound(v,x) lower_bound(v.begin(), v.end(), x)-v.begin()
+#define upperbound(v,x) upper_bound(v.begin(), v.end(), x)-v.begin()
+
+#define pb push_back
+#define loj(i,j) "Case "<<i<<": "<<j
+#define gap " "
+
+#define auto(x,a) for (auto& x : a)
+#define print(x) cout << #x << " = " << x << endl
+
+#define moveForward(current, ammount, mod) (amount - current) % mod
+#define moveBackward(current, ammount, mod) (((amount - current) % mod) + mod) % mod
+
+long long dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+long long dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    
+   ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
+
     int t;
-    std::cin >> t;
-    
+    cin >> t;
     while (t--) {
-        solve();
+        int n, an; cin>>n;
+        string s; cin>>s;
+        an=n;
+
+        for(int i=1;i<=n;i++){
+            if(n%i) continue;
+            bool f=false;
+
+            for(int j=0;j<n && j<=i; j+=i){
+                int ans = 0;
+                for(int k=0;k<n;k++){
+                    ans+=(s[j+ k % i]!=s[k]);
+                }
+                if(ans<=1){
+                    f=true;
+                    an = i;
+                    break;
+                }
+            }
+            if(f) break;
+        }
+        cout<<an<<endl;
     }
-    
-    return 0;
 }
