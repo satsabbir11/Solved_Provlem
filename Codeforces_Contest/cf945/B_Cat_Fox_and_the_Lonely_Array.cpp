4818b4400fbf8c2bf771fcdcfb269d3ee0b58efe
tsabbir007 <satsabbir11@gmail.com>
2024-05-24 02:36:02 +0600

    Accepted

--- Codeforces_Contest/cf945/B_Cat_Fox_and_the_Lonely_Array.cpp
+++ Codeforces_Contest/cf945/B_Cat_Fox_and_the_Lonely_Array.cpp
@@ -1,36 +1,79 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void keepBit(vector<int> &b, int n, int h = 1)
+{
+    int i = 25;
+    for (i = 25; i >= 0; i--)
+    {
+        if (n >= (2 << i))
+        {
+            n -= (2 << i);
+            b[i + 1] = b[i + 1] + h;
+        }
+    }
+    if (n)
+        b[i + 1] = b[i + 1] + h;
+}
 
-int main() {
-   ios_base::sync_with_stdio(false); cin.tie(0),cout.tie(0);
+int number(vector<int> &b)
+{
+    int ans = 0;
+    for (int i = 0; i < b.size(); i++)
+    {
+        if (b[i])
+        {
+            ans += pow(2, i);
+        }
+    }
+    return ans;
+}
 
-    long long t;
+bool check(vector<int> &a, int k, int n)
+{
+    vector<int> b(32);
+    for (int i = 1; i <= k; i++)
+    {
+        keepBit(b, a[i]); // first we keep 1 to k
+    }
+
+    int ans = number(b);
+
+    for (int i = k + 1; i <= n; i++)
+    {
+        keepBit(b, a[i]);
+        keepBit(b, a[i - k], -1); // slideing window
+
+        if (ans != number(b)) return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int t;
     cin >> t;
-    while (t--) {
-        long long n;
-        cin>>n;
-        
-        long long a[n+2], ans = n, cumOr[n+2]={};
-        for(long long i=1;i<=n;i++){
-            cin>>a[i];
-            if(i==1) cumOr[i]=a[i];
-            else cumOr[i] = cumOr[i-1] | a[i];
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n + 2);
+        for (int i = 0; i < n; i++)
+            cin >> a[i + 1];
+
+        int c = n;
+
+        int l=1, r=n;
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(check(a, mid, n)) r=mid;
+            else l=mid+1;
         }
 
-        for(long long i=3;i<=3;i++){
-            set<long long>st;
-            for(long long j=1; j<=n-i+1;j++){
-                if(j+i-1 > n) break;
-                long long orValue = cumOr[j+i-1] ^ cumOr[j-1];
-                cout<<cumOr[j+i-1]<<" "<<cumOr[j-1]<<" "<<orValue<<" "<<i<<endl;
-                st.insert(orValue);
-                //if(st.size()!=1) break;
-            }
-            cout<<endl;
-            if(st.size()==1) ans = min(i, ans);
-        }
-
-        cout<<ans<<endl;
+        cout <<l<< endl;
     }
 }
